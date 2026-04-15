9c866ccfab9e9011b623ffdac19313e61978de93
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2026-04-15 14:59:37 -0300

    Adicionando icone de logOut do lucide

--- app/components/ui/sign-out.tsx
+++ app/components/ui/sign-out.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { LogOut } from "lucide-react";
 import { useAuth } from "@/hooks/useAuth";
 import { Button } from "@/components/ui/button";
 
 
 const ButtonLogout = () => {
 	const { logout } = useAuth();
-	return <Button onClick={() => logout()}>Encerrar sessão</Button>;
+	return (
+		<Button onClick={() => logout()}>
+			<LogOut className="mr-2 h-4 w-4" /> Sair
+		</Button>
+	);
 };
 
 export { ButtonLogout };
