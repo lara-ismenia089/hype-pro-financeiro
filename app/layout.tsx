4cca0d27e8cdc1eace177b992fa133102ac4e519
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2026-04-15 11:59:38 -0300

    Adicionando o toast no layout raiz do projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/sonner";
+
+
 const inter = Inter({
   subsets: ["latin"],
   display: 'swap',
@@ -38,6 +41,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
       >
         {children}
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   );
