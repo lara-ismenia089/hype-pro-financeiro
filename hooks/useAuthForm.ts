e0c159d86827d1bcc046ab515d8990411975a5b9
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2026-04-15 11:58:43 -0300

    Hook de login e register

---
+++ hooks/useAuthForm.ts
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+import { toast } from "sonner";
+
+import {
+	signIn,
+	signUp,
+} from "@/lib/auth/auth-client";
+
+import {
+	loginSchema,
+	registerSchema,
+} from "@/lib/validations/authSchemas";
+
+import { ROUTER } from "@/lib/constants/routes";
+
+
+export function useAuthForm() {
+	const [error, setError] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const login = async (values: { email: string, password: string }) => {
+		const parsed = loginSchema.safeParse(values);
+		if (!parsed.success) {
+			setError(parsed.error.message);
+			return;
+		}
+
+		signIn.email({
+			email: parsed.data.email,
+			password: parsed.data.password,
+			callbackURL: ROUTER.dashboard,
+			rememberMe: false,
+		}, {
+			onRequest() {
+				setIsLoading(true);
+			},
+			onSuccess() {
+				setError(null);
+				setIsLoading(false);
+			},
+			onError(context) {
+				toast.error("E-mail ou senha incorretos");
+				setError(context.error.message);
+				setIsLoading(false);
+			},
+		});
+	};
+
+	const register = async (values: { name: string, email: string, password: string }) => {
+		const parsed = registerSchema.safeParse(values);
+		if (!parsed.success) {
+			setError(parsed.error.message);
+			return;
+		}
+
+		signUp.email({
+			name: parsed.data.name,
+			email: parsed.data.email,
+			password: parsed.data.password,
+			callbackURL: ROUTER.login,
+			
+		}, {
+			onRequest() {
+				setIsLoading(true);
+			},
+			onSuccess() {
+				setError(null);
+				setIsLoading(false);
+			},
+			onError(context) {
+				toast.error("Informe dados válidos.");
+				setError(context.error.message);
+				setIsLoading(false);
+			},
+		});
+	};
+
+	return { login, register, error, isLoading };
+}
