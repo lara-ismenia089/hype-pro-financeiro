ab2ac2d8949944c84cf522491c5ddc6308a6bbf2
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2026-05-19 08:12:45 -0300

    configuração do projeto

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,49 @@
+import js from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+export default defineConfig([
+	js.configs.recommended,
+
+	...nextVitals,
+	...nextTs,
+
+	{
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+
+		rules: {
+			"simple-import-sort/imports": [
+				"error",
+				{
+					groups: [
+						// React / Next / libs externas
+						["^react$", "^next", "^@?\\w"],
+
+						// Aliases internos
+						["^@/"],
+
+						// Relativos
+						["^\\."],
+
+						// Side effects
+						["^\\u0000"],
+					],
+				},
+			],
+
+			"simple-import-sort/exports": "error",
+		},
+	},
+
+	globalIgnores([
+		".next/**",
+		"out/**",
+		"build/**",
+		"next-env.d.ts",
+	]),
 ]);
-
-export default eslintConfig;
